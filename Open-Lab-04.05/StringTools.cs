c9e7d203140b584f3c1b18c0d9d9d408ee526400
MilosM33 <milosmarejka89@gmail.com>
2020-10-08 20:59:51 +0200

    init

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string temp = "";
+            for (int y= 0;  y< orig.Length; y++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    temp += orig[y];
+                }
+            }
+            return temp;
         }
     }
 }
